17d62766f6131793563768781b045915ef680fb0
oneko <onekorama@gmail.com>
2021-05-31 00:39:21 +0200

    Enabled organization repositories Sourcemaps optional

--- script.sh
+++ script.sh
@@ -6,15 +6,18 @@ export SENTRY_PROJECT=${PLUGIN_PROJECT}
 export SENTRY_ENVIRONMENT=${PLUGIN_ENVIRONMENT}
 export SENTRY_RELEASE=${DRONE_COMMIT_SHA}
 
-git clone https://${PLUGIN_REPO_USER}:{$PLUGIN_REPO_TOKEN}@github.com/${PLUGIN_REPO_USER}/${PLUGIN_REPO_NAME}.git && cd ${DRONE_REPO_NAME}
-git remote set-url origin https://${PLUGIN_REPO_USER}:{$PLUGIN_REPO_TOKEN}@github.com/${PLUGIN_REPO_USER}/${PLUGIN_REPO_NAME}.git
+git clone https://${PLUGIN_REPO_USER}:${PLUGIN_REPO_TOKEN}@github.com/${PLUGIN_REPO_ORG}/${DRONE_REPO_NAME}.git && cd ${DRONE_REPO_NAME}
+git remote set-url origin https://${PLUGIN_REPO_USER}:${PLUGIN_REPO_TOKEN}@github.com/${PLUGIN_REPO_ORG}/${DRONE_REPO_NAME}.git
 git checkout ${DRONE_SOURCE_BRANCH}
 echo "Creando release ${SENTRY_RELEASE}"
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 echo "Commits para ${SENTRY_RELEASE}"
 sentry-cli releases set-commits $SENTRY_RELEASE --auto
-echo "Sourcemaps para ${SENTRY_RELEASE}"
-sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps src
+if [ "$PLUGIN_SOURCEMAPS" == "true" ]
+then
+  echo "Sourcemaps para ${SENTRY_RELEASE}"
+  sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps src
+fi
 echo "Finalizando release ${SENTRY_RELEASE}"
 sentry-cli releases finalize $SENTRY_RELEASE
 echo "Release ${SENTRY_RELEASE} desplegada  "
